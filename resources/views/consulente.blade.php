a5cd4a0468137d6ecb2487cb50341bdba7ac4008
Ivan Maeran <39743980+Odinozz@users.noreply.github.com>
2023-10-12 10:53:35 +0200

    Add files via upload

--- resources/views/consulente.blade.php
+++ resources/views/consulente.blade.php
@@ -1,11 +1,22 @@
 <x-app-layout>
-    <section class="mx-[5vw]">
-        <div class="py-16 md:py-20">
-            <h3 class="text-secondary text-center font-bold mx-auto text-2xl md:text-3xl max-w-[500px]">
-                La sezione Consulente AI è ancora in sviluppo...
-            </h3>
-            <img class="mx-auto w-[80vw] mt-12 max-w-[700px] md:mt-16" src="/img/work_in_progress.svg" alt="">
-        </div>
-    </section>
+    <div id="chat-container">
+        @foreach ($chatLog as $message)
+            @if ($message['role'] === 'user')
+                <div class="user-message">
+                    <div class="message-bubble">{{ $message['message'] }}</div>
+                </div>
+            @else
+                <div class="bot-message">
+                    <div class="message-bubble">{{ $message['message'] }}</div>
+                </div>
+            @endif
+        @endforeach
+    </div>
+    <form method="POST" action="{{ route('consulente.reply') }}">
+        @csrf
+        <input type="text" name="message" placeholder="Type your message...">
+        <button type="submit">Send</button>
+    </form>
+    <link href="{{ asset('css/chat.css') }}" rel="stylesheet">
     @livewireScripts
 </x-app-layout>
